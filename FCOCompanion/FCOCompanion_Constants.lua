d951332e30f1a6c10209637bde75f8793915f692
Baertram <pz1@gmx.net>
2021-06-11 00:32:24 +0200

    Increased version

--- FCOCompanion/FCOCompanion_Constants.lua
+++ FCOCompanion/FCOCompanion_Constants.lua
@@ -5,7 +5,7 @@ local FCOCompanion = FCOCO
 --[Addon variables]
 FCOCompanion.addonVars = {}
 local addonVars = FCOCompanion.addonVars
-addonVars.addonVersion		        = 0.05
+addonVars.addonVersion		        = 0.06
 addonVars.addonSavedVarsVersion	    = "0.04"
 addonVars.addonSavedVarsForAllTable = "SettingsForAll"
 addonVars.addonSavedVarsNormalTable = "Settings"
